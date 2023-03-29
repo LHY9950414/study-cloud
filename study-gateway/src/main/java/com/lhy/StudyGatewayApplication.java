378ce6b5fe2425bb713ddaf9ab60e075fe8f0a52
liheyan <1161643430@qq.com>
2023-03-29 22:13:04 +0800

    1: 添加启动类

---
+++ study-gateway/src/main/java/com/lhy/StudyGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.lhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liheyan
+ */
+@SpringBootApplication
+public class StudyGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGatewayApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  时光-网关启动成功   ლ(´ڡ`ლ)ﾞ ");
+    }
+}
